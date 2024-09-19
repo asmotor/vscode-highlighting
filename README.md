dedf37a00fe6f24cb37470910cf1b2f597688bd0
Carsten Elton Sørensen <cso@rift.dk>
2024-09-19 22:35:45 +0200

    Update readme, little cleanup

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # asmotor-syntax README
 
-This VSCode extension provides syntax highlighting for the ASMotor macro assembler family.
-
 ASMotor is a collection of assemblers plus a linker and a librarian. The package supports the CPU families
-MOS 6502, Motorola 680x0, Z80 and Game Boy, MIPS32, the FPGA-only RC800 and the virtual DCPU-16. The linker supports several
-output formats, such as Amiga hunk, Game Boy, Commodore PRG, Sega Master System and more.
+MOS 6502, Motorola 680x0, Z80 and Game Boy, MIPS32, the FPGA-only RC800 and the virtual DCPU-16. The linker supports several output formats, such as Amiga hunk, Game Boy, Commodore PRG, Sega Master System and more.
+
+This VSCode extension provides syntax highlighting and language support for the ASMotor macro assembler family.
+
+The extension supports the following functionality:
+
+* Go to symbol in editor (Ctrl+Shift+O on Linux)
+* Go to definition (F12)
+* Peek definition (Ctrl+Shift+F10)
+* Display definition to the side (Ctrl+K F12)
+* Switch header/source (Alt+O)
+
+Please note that other language extensions may also have registered the Alt+O shortcut, and you may have to correct their "when" clause in the keybinding editor. For clangd, the when clause should be `(resourceLangId == c || resourceLangId == cpp || resourceLangId == cuda-cpp || resourceLangId == objective-c || resourceLangId == objective-cpp) && editorTextFocus`.
 
 ASMotor must be installed separately. It can be found at https://github.com/asmotor/asmotor
 
--- src/switch_source_header.ts
+++ src/switch_source_header.ts
@@ -7,35 +7,15 @@ export async function switchSourceHeader(context: vscode.ExtensionContext): Prom
 	if (!vscode.window.activeTextEditor)
 		return;
 
-//	const path = require('path') as path;
-
+	// Find files with the same basename
 	const filename = path.basename(vscode.window.activeTextEditor.document.fileName);
-	const basename = path.basename(filename, path.extname(filename));
-	const pattern = "**/" + basename + ".{" + fileEndings + "}";
-	const files = await vscode.workspace.findFiles(pattern);
-	const sorted = files.sort();
-	const index = sorted.map(v => path.basename(v.path)).indexOf(filename);
+	const pattern = "**/" + path.basename(filename, path.extname(filename)) + ".{" + fileEndings + "}";
+	const files = (await vscode.workspace.findFiles(pattern)).sort();
+
+	// Switch to the next similarly named file
+	const index = files.map(v => path.basename(v.path)).indexOf(filename);
 	if (index != -1) {
-		const nextDocument = sorted[(index + 1) % sorted.length];
-		const textDocument = await vscode.workspace.openTextDocument(nextDocument);
+		const textDocument = await vscode.workspace.openTextDocument(files[(index + 1) % files.length]);
 		vscode.window.showTextDocument(textDocument);
 	}
-
-
-	/*
-	let dotPosition = filename.lastIndexOf(".");
-	let dotPosition = filename.lastIndexOf(".");
-
-
-	const docIdentifier = vscodelc.TextDocumentIdentifier.create(uri.toString());
-	const sourceUri =
-		await client.sendRequest(SwitchSourceHeaderRequest.type, docIdentifier);
-	if (!sourceUri) {
-	vscode.window.showInformationMessage('Didn\'t find a corresponding file.');
-	return;
-	}
-	const doc =
-		await vscode.workspace.openTextDocument(vscode.Uri.parse(sourceUri));
-	vscode.window.showTextDocument(doc);
-	*/
 }
